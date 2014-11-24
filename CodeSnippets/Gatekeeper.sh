143a539619698806edfc8a91e1c74ea238919c27
Thomas Dreibholz <dreibh@simula.no>
2014-11-24 12:56:26 +0100

    Some improvements.

--- CodeSnippets/Gatekeeper.sh
+++ CodeSnippets/Gatekeeper.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-USER=ola
-KEY=~/.ssh/gatekeeper
+USER=ola                # <- The username for the Gatekeeper server
+KEY=~/.ssh/gatekeeper   # <- The private key file for authentication
 
-ssh -i $KEY $USER@gatekeeper.nntb.no \
+ssh -i ${KEY} ${USER}@gatekeeper.nntb.no \
  -L 2000:plc.simula.nornet:443 \
  -L 2001:monitor.simula.nornet:80
