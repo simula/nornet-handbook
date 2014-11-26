6e1d2bb2a461ff469c12c43d46fa3b06afde9683
Thomas Dreibholz <dreibh@simula.no>
2014-11-26 16:21:18 +0100

    Some updates.

--- CodeSnippets/Gatekeeper.sh
+++ CodeSnippets/Gatekeeper.sh
@@ -5,4 +5,5 @@ KEY=~/.ssh/gatekeeper   # <- The private key file for authentication
 
 ssh -i ${KEY} ${USER}@gatekeeper.nntb.no \
  -L 2000:plc.simula.nornet:443 \
- -L 2001:monitor.simula.nornet:80
+ -L 2001:monitor.simula.nornet:80 \
+ -L 2002:perfmon1.simula.nornet:80
